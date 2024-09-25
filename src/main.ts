ba27a83c8f23d72184b0e64c01a946013dc6ecb2
khallad2 <khallad_3@hotmail.com>
2024-09-25 09:24:23 +0200

    feat: configure app, set api prefix, enable cors,

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
-async function bootstrap() {
+export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors();
+
+  // Global validation for request payloads
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Strip out properties that aren't in the DTO
+      forbidNonWhitelisted: true, // Throw an error if there are unexpected properties
+      transform: true, // Automatically transform payloads to match DTO classes
+    }),
+  );
+
+  // Global route prefix (e.g., /api/your-route)
+  app.setGlobalPrefix('api');
+
+  // Enable graceful shutdown hooks
+  app.enableShutdownHooks();
+
   await app.listen(3000);
 }
 bootstrap();
